06414320c26c1bea2a881b6e8c92beba8a787674
jonwyun <113542871+jonwyun@users.noreply.github.com>
2023-09-09 01:50:36 -0400

    updating hero image part

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Burger Menu
+
+let button = document.querySelector("#button");
+let burgerCon = document.querySelector("#burger-con");
+
+function burgerMenu() {
+    burgerCon.classList.toggle("slide-toggle");
+    button.classList.toggle("expanded");
+};
+
+button.addEventListener("click", burgerMenu, false);
